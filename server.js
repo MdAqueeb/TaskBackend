f40630c0c421205a0a3ac2c2b65e2217dce14ab0
MdAqueeb <aqueebahmed505@gmail.com>
2025-07-23 13:53:28 +0530

    modified mongoDB connection URI

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ connectDB();
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use('/api', userRoutes);
-app.use('/api', historyRoutes);
+app.use(userRoutes);
+app.use(historyRoutes);
 
 // app.use("/.netlify/functions/app", router);
 // module.exports.handler = serverless(app);
