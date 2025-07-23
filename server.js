68aa979a1a328491eb0b3112df6dc77dccc55946
MdAqueeb <aqueebahmed505@gmail.com>
2025-07-23 13:46:07 +0530

    modified mongoDB connection URI

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://leaderboard-aqueeb.netlify.app/',    
+  origin: 'https://leaderboard-aqueeb.netlify.app',    
   credentials: true                         
 }));
 
