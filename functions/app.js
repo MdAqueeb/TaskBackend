e6f96d12c8314974492666af9752688faa33f7b5
MdAqueeb <aqueebahmed505@gmail.com>
2025-07-23 18:21:20 +0530

    test

--- functions/app.js
+++ functions/app.js
@@ -1,24 +1,28 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('../routes/users.js');
-const historyRoutes = require('../routes/history.js');
-const connectDB = require('../config/db.js');
-const cors = require('cors');
+const serverless = require("serverless-http");
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("../routes/users.js");
+const historyRoutes = require("../routes/history.js");
+const connectDB = require("../config/db.js");
+const cors = require("cors");
 
 const app = express();
+const router = express.Router();
 
 connectDB();
 
-app.use(cors({
-  origin: 'https://leaderboard-aqueeb.netlify.app',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://leaderboard-aqueeb.netlify.app",
+    credentials: true,
+  })
+);
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use('/users', userRoutes);
-app.use('/history', historyRoutes);
+app.use("/users", userRoutes);
+app.use("/history", historyRoutes);
 
+app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
